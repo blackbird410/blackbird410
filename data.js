adf34bdc1846662bac0b840d68ceb6cc0e24d938
Neil T. Rigaud <ntsrigaud14@gmail.com>
2024-01-24 13:16:47 +0800

    Removed unnecessary thing

--- data.js
+++ data.js
@@ -121,7 +121,6 @@ export const myCertifs = [
     "./page_img/certificates/CS50P Certificate.png",
     "./page_img/certificates/RWD_cert.png",
     "./page_img/certificates/SciPy_cert.png",
-    "./page_img/certificates/toeic.png"
 ];
 
 export function addHeader(container, myLinks) {
