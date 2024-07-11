0354d3465227f60761a421855aa3611265a3772b
Neil T. Rigaud <ntsrigaud14@gmail.com>
2024-07-11 15:19:39 +0800

    Add homepage and project page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,16 +1,12 @@
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
-
-const links = [
-    { title: "Home", href: "/", isSelected: true, },
-    { title: "About", href: "/about", isSelected: false, },
-    { title: "Contact", href: "/contact", isSelected: false, },
-];
+import { getLinks } from "./Homepage";
 
 export default function ErrorPage() {
+
     return (
             <div className="">
-                <Navbar links={links} />
+                <Navbar links={getLinks("ErrorPage")} />
                 <p>Sorry, this page is not available!</p>
                 <Link to={"/"}>Go back home</Link>
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,36 @@
+import { document } from "postcss";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
-function Icon({ type }) {
-    return (
-        <>
-            {type === "Menu" && 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
-                </svg>
-            }
-
-            {type === "Home" && 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 ml-2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"></path>
-                </svg>
-            }
-
-            {type === "About" && 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 ml-2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"></path>
-                </svg>
-            }
-
-            {type === "Contact" && 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 ml-2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"></path>
-                </svg>
-            }
-        </>
-    );
-};
+import Icon from "./Icon";
 
 function Navlink ({title, href, isSelected}) {
     const selected = "text-gray-700 font-bold border-r-4 border-primary";
     return (
         <li 
             className={isSelected ? selected : "py-1 border-r-4 border-white"}>
-            <Link to={href} className="px-4 flex justify-end"><span>{title}</span>
-            </Link>
+            <Link 
+                to={href} 
+                className={"px-4 flex justify-end"}
+            ><span>{title}</span>
             <Icon type={title} />
+            </Link>
         </li>
     );
 }
 
 export default function Navbar({ links }) {
+    const [isNavOpen, setIsNavOpen] = useState(false);
 
     return (
-        <div className="md:col-span-1 md:flex- md:justify-end">
-            <nav className="text-right">
+        <div className="md:col-span-1 md:flex md:justify-end">
+            <nav className={`text-right`}>
                 <div className="flex justify-between items-center">
                     <h1 className="font-bold p-4 border-b border-gray-100">Neil</h1>
-                    <div id="burger" className="px-4 cursor-pointer md:hidden">
+                    <div id="burger" className="px-4 cursor-pointer md:hidden" onClick={() => setIsNavOpen(!isNavOpen)}>
                         <Icon type={"Menu"}/>
                     </div>
                 </div>
-                <ul id="navbar" className="text-sm mt-6 hidden md:block">
+                <ul className={`text-sm mt-6 md:block ${isNavOpen ? 'hidden' : ''}`}>
                     {links.map(link => 
                         <Navlink 
                             key={link.title} 
